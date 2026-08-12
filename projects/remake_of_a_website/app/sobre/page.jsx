20e1dd7bb505475cd5cfc4ae2e4740fec10593db
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-08-12 16:19:12 -0300

    Finished a simple 3 paged website, although it isn't the highsest uality of the world xd

--- projects/remake_of_a_website/app/sobre/page.jsx
+++ projects/remake_of_a_website/app/sobre/page.jsx
@@ -5,7 +5,7 @@ export default function sobre(){
 	<>
 		<div className="cont_simples">
 			<h1>Sobre este sitio web</h1>
-<p>Inseir muchas palabras aqui inserir muchas palabras aqui</p>
+<p>Haciendo lo mismo somente por diverson, no sé se esperaba algo como de una empresa de verdad, pero se fué eso estás en el lugar errado XD</p>
 		</div>
 	</>
 	);
