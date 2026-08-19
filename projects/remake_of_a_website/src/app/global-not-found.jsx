59c9d144c4c9b70d189ce113492ab42ab7fd277f
Ramalh0VT <gustavoramalhodasilva777@gmail.com>
2026-08-19 14:34:54 -0300

    Updated the application

---
+++ projects/remake_of_a_website/src/app/global-not-found.jsx
@@ -0,0 +1,14 @@
+export default function notFound(){
+	return <>
+		<html>
+		<body>
+		<div className="container-notfound">
+			<div className="flex-notfound">
+				<h2>ERRO 404</h2>
+				<p>Página não encontrada</p>
+			</div>
+		</div>
+		</body>
+		</html>
+	</>
+}
